5ef9df6c48b3a0dff8a01c0ca285f7abfcd870de
ideepak3m <ivdeepak@gmail.com>
2025-09-22 11:25:50 -0400

    Added base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/priority-one-ai-launch/',
   server: {
     host: "::",
     port: 8080,
